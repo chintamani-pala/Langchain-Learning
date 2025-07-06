728fd97ea6b2c7f2ed6791d1ac8797132a0d6fef
chintamani pala <chintamanipala67@gmail.com>
2025-07-06 22:44:46 +0530

    AI Agent ReAct based architecture

---
+++ campusx/Agent/ReAct_agent.py
@@ -0,0 +1,23 @@
+# type: ignore
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_community.tools import DuckDuckGoSearchRun
+from langsmith import Client
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+client = Client(api_key=os.getenv("LANGSMITH_API_KEY"))
+prompt = client.pull_prompt("hwchase17/react:d15fe3c4", include_model=True)
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+search_tool = DuckDuckGoSearchRun()
+
+agent = create_react_agent(llm=llm, tools=[search_tool], prompt=prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=[search_tool], verbose=True)
+
+response = agent_executor.invoke(
+    {"input": "What are the 3 ways to reach chennai from banglore"}
+)
+print(response)
